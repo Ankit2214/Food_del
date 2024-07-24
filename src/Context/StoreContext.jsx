1e25cbd65117b4ddd0af055791f3893c6b485415
Ankitt2214 <kumarankii.2214@gmail.com>
2024-07-24 22:10:39 +0530

    commited

--- src/Context/StoreContext.jsx
+++ src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "http://localhost:4000"
+    const url = "https://food-del-server-2kmx.onrender.com:4000"
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
